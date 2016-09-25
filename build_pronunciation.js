ee907b92c4c23531597189f1c94699cd06b41e14
Don Kirkby <donkirkby@gmail.com>
2016-09-25 16:51:36 -0700

    Work toward #1 by dumping 10000 character pronunciations.

--- build_pronunciation.js
+++ build_pronunciation.js
@@ -7,10 +7,23 @@ fs.readFile('cedict.script', {encoding: 'utf-8'}, function(err, data){
         console.log(err);
     } else {
         var re = /^INSERT INTO CHARACTER VALUES\(\d+,'(.*)','(.*)','(.*)','.*'\)$/gm,
-            count = 0;
-        while ((result = re.exec(data)) && count < 100) {
-            console.log(result[1] + ': ' + result[3]);
-            count += 1;
+            characters = new Set(),
+            line = "";
+        while ((result = re.exec(data)) && characters.size < 10000) {
+            for (var i = 2; i <= 3; i++) {
+                var json = '"' + result[i] + '"';
+                result[i] = JSON.parse(json);
+            }
+            if (result[2].length == 1) {
+                if ( ! characters.has(result[2])) {
+                    line += '"' + result[2] + '":"' + result[3] + '",';
+                    characters.add(result[2]);
+                    if (characters.size % 6 === 0) {
+                        console.log(line);
+                        line = "";
+                    }
+                }
+            }
         }
     }
 });
