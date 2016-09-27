20f47016d095af039f66808fc724149935ceaeb2
Zhuyi Xue <alfred532008@gmail.com>
2016-09-26 21:41:00 -0700

    used es6 style import in pronunciation.js

--- src/pronunciation.js
+++ src/pronunciation.js
@@ -1,5 +1,5 @@
 // parsed from https://sourceforge.net/p/zdt/svn/HEAD/tree/tags/0.7.0b3/net.sourceforge.zdt.cedict/cedict.script
-var chars = require('./chars.json');
+import chars from './chars.json';
 
 
 var pronunciation = (function() {
